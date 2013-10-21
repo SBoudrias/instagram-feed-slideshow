3540fb98771d72c81c07d394b253e34c2fb215ca
Simon Boudrias <simon.boudrias@cossette.com>
2013-10-21 15:13:34 -0400

    test hashtag

--- www/config.js
+++ www/config.js
@@ -4,7 +4,7 @@ var config = {
 		secret : "e10c09a2ee73453fb719be5875a4ef42",
 		callbackUrl: "http://cossette-halloween.herokuapp.com/newphoto"
 	},
-	hashtag: 'bonparty'
+	hashtag: 'k7foo'
 };
 
 module.exports = config;
