8ee8675fe4ebd64196068627eecd796c6e6a92f4
Simon Boudrias <simon.boudrias@cossette.com>
2013-10-21 17:33:33 -0400

    bug

--- www/app.js
+++ www/app.js
@@ -89,9 +89,9 @@ ioServer.sockets.on('connection', function( socket ) {
 // ---
 // Subscribe to instagram
 
-Instagram.media.unsubscribe_all();
+Instagram.media.unsubscribe_all({});
 process.on('SIGINT', function() {
-	Instagram.media.unsubscribe_all();
+	Instagram.media.unsubscribe_all({});
 });
 Instagram.subscriptions.subscribe({ object: 'tag', object_id: config.hashtag });
 
